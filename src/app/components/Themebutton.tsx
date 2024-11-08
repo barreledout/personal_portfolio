716beaf36aee5971e4c6b00dbf272b5e734c92dc
barreledout <rcalisaan7@gmail.com>
2024-11-07 23:56:56 -0800

    Use next-theme package to handle theme switch. I also added a sound effect for the theme switcher with use-sound package.

--- src/app/components/Themebutton.tsx
+++ src/app/components/Themebutton.tsx
@@ -1,35 +1,59 @@
 "use client";
-import { IoSunnyOutline } from "react-icons/io5";
-import { IoMoonOutline } from "react-icons/io5";
 import { useState, useEffect } from "react";
-import { TbJetpack } from "react-icons/tb";
+import { useTheme } from "next-themes";
 
-const Themebutton: React.FC = () => {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+import useSound from "use-sound";
+
+import { IoSunnyOutline } from "react-icons/io5";
+import { LuMoon } from "react-icons/lu";
+
+const Themebutton = () => {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
+  const [playOn] = useSound("/sound/btnClick2.mp3", { volume: 1 });
+  const [playOff] = useSound("/sound/btnClick1.mp3", { volume: 1 });
 
   useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    const theme = localStorage.getItem("theme");
+
     if (theme === "dark") {
-      document.querySelector("html")?.setAttribute("data-theme", "dark");
-    } else {
-      document.querySelector("html")?.removeAttribute("data-theme");
+      return <IoSunnyOutline className="w-[40px] h-[50px] " />;
+    } else if (theme === "light" || "null") {
+      return <LuMoon className="w-[40px] h-[40px]" />;
     }
-  }, [theme]);
+  }
 
   const toggleTheme = () => {
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
-    localStorage.setItem("theme", newTheme);
+    if (resolvedTheme === "light") {
+      setTheme("dark");
+      playOn();
+    }
+
+    if (resolvedTheme === "dark") {
+      setTheme("light");
+      playOff();
+    }
   };
 
   return (
     <button
       onClick={toggleTheme}
-      className="flex justify-center items-center bg-transparent border-[none] w-[40px] h-[40px] rounded-[10px] cursor-pointer pt-[2px] [transition:all_0.3s_ease] hover:bg-hover-hoverColor"
+      className="flex justify-center items-center bg-transparent border-[none] w-[40px] h-[40px] cursor-pointer"
     >
-      {theme === "light" ? (
-        <IoMoonOutline className="text-fontColor" style={{ fontSize: "28px" }} />
+      {resolvedTheme === "light" ? (
+        <LuMoon
+          className="text-fontColor [transition:color_0.2s_ease-in-out] hover:text-purple-500"
+          style={{ fontSize: "25px" }}
+        />
       ) : (
-        <IoSunnyOutline className="text-fontColor" style={{ fontSize: "30px" }} />
+        <IoSunnyOutline
+          className="text-custom-fontColor [transition:color_0.2s_ease-in-out] pt-[2px] hover:text-yellow-500"
+          style={{ fontSize: "30px" }}
+        />
       )}
     </button>
   );
