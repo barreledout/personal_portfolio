043f17e42847df9b983131007538a059f6e1c9c2
barreledout <rcalisaan7@gmail.com>
2024-11-08 12:24:44 -0800

    Added framer motion

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,12 +2,23 @@
 import { TiLocationArrowOutline } from "react-icons/ti";
 import { GitHub, Email, Resume } from "./SocialsComp";
 import { PageWrapper } from "./FramerMotion/PageWrapper";
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: { filter: "blur(10px)", transform: "translateY(20%)", opacity: 0 },
+  visible: { filter: "blur(0)", transform: "translateY(0)", opacity: 1 },
+};
 
 const Hero: React.FC = () => {
   return (
     <div className="mt-[4rem] pb-[50px] mx-auto w-[650px] items-center">
-      <PageWrapper delayAmt={0.3}>
-        <div className="inline-flex gap-5 items-center  ">
+      <PageWrapper>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          className="inline-flex gap-5 items-center"
+        >
           <img
             src="/heroPic.jpeg"
             alt="Profile Picture"
@@ -23,8 +34,13 @@ const Hero: React.FC = () => {
               </span>
             </span>
           </div>
-        </div>
-        <div className="mt-[2rem] flex flex-col ">
+        </motion.div>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          className="mt-[2rem] flex flex-col "
+        >
           <h1 className="text-[1.2em] pb-[5px] font-[500] border-b-transparent relative after:content=[''] after:absolute after:top-7 after:left-0 after:border-b after:border-[#9CA3AF] after:w-[85px] ">
             About Me
           </h1>
@@ -38,7 +54,7 @@ const Hero: React.FC = () => {
             </span>
             . The goal of this website is to not only showcase my projects.{" "}
           </p>
-        </div>
+        </motion.div>
         <div className="inline-flex items-center gap-3 pt-2 ">
           <GitHub IconClassName="text-[#9CA3AF] hover:text-hover-textHoverWhite/80 ml-[-1px]" />
           <Email IconClassName="text-[#9CA3AF] text-[27px] hover:text-hover-textHoverWhite/80 ml-[-1px]" />
