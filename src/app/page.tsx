19c57ef1fbb6d14bf9183651bfbc0bd2ac70b4ec
barreledout <rcalisaan7@gmail.com>
2024-11-08 23:26:31 -0800

    The only children of the page.tsx are components.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,10 @@ import Techstack from "./components/Techstack";
 
 export default function Home() {
   return (
-    <div>
+    <div className={`w-[650px] text-custom-fontColor`}>
       <Hero />
-      <div className="flex flex-col justify-center text-center mt-[2rem]">
-        <Techstack />
-      </div>
+      <Techstack />
       <Projects />
-      <div>hello</div>
     </div>
   );
 }
