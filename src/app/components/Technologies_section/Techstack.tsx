e6f749bfee3ffb4341411268d98675a3662fded3
barreledout <rcalisaan7@gmail.com>
2024-11-11 22:33:23 -0800

    Implementing dialog (modal) ShadCN component for the view more section of the tech stack section. Also added popover component

--- src/app/components/Technologies_section/Techstack.tsx
+++ src/app/components/Technologies_section/Techstack.tsx
@@ -1,15 +1,26 @@
 "use client";
-import { motion, useDragControls } from "framer-motion";
+import { motion } from "framer-motion";
 import { variants } from "../FramerMotion/PageWrapper";
 import Cards from "./ProjectCards";
+import { FaArrowRightLong } from "react-icons/fa6";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ShadCn/dialog";
 
 const Techstack = () => {
-  const stack: [string, string][] = [
+  const CurrentStack: [string, string][] = [
     ["typescript", "1 month"],
     ["react", "1 month"],
     ["tailwindcss", "1 month"],
     ["nextjs", "1 month"],
   ];
+
+  const OtherStack: [string, string][] = [];
 
   return (
     <motion.div
@@ -17,10 +28,25 @@ const Techstack = () => {
       className="font-[400] text-custom-fontColor"
     >
       {/* possibly create a 'view more' to show more languages I know that popups a modal or new route? */}
-      <h1 className="text-[1.4em] font-[500] py-5">Current Tech Stack</h1>
+      <div className="flex justify-between items-center relative">
+        <h1 className="text-[1.4em] font-[500] py-5">Current Tech Stack</h1>
+        <Dialog>
+          <DialogTrigger className="flex items-center gap-2 ">
+            View More <FaArrowRightLong />
+          </DialogTrigger>
+          <DialogContent className="flex flex-col justify-center bg-custom-containerColor text-custom-fontColor dark:border-0 ">
+            <DialogHeader>
+              <DialogTitle className="text-center">
+                Other Technologies
+              </DialogTitle>
+            </DialogHeader>
+            <div className="grid grid-cols-3"></div>
+          </DialogContent>
+        </Dialog>
+      </div>
 
       <div className="grid grid-cols-3 items-center gap-2">
-        {stack.map(([lang, exp], index) => (
+        {CurrentStack.map(([lang, exp], index) => (
           <div key={index}>
             <Cards language={lang} exp={exp} />
           </div>
@@ -31,3 +57,7 @@ const Techstack = () => {
 };
 
 export default Techstack;
+
+{
+  /* <button className="flex items-center gap-2"></button> */
+}
