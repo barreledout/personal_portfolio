d838008bea43f9be3e43ccf3c10fcc691f083ade
barreledout <rcalisaan7@gmail.com>
2024-11-13 23:02:11 -0800

    Finished the drawer feature for the project cards. Also added framer motion to the parent element.

--- src/app/components/Projects/ProjectCards.tsx
+++ src/app/components/Projects/ProjectCards.tsx
@@ -1,66 +1,138 @@
 "use client";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState, useEffect, SyntheticEvent } from "react";
 import { cn } from "@/lib/utils";
 import { Button } from "../ShadCn/button";
 
 import { Drawer } from "vaul";
 
 interface ProjectProps {
-  children: React.ReactNode;
+  children: {
+    description: React.ReactNode;
+  };
   alt: string;
   img: string;
-  link: string;
+  gitHubLink: string;
+  demoLink?: string;
 }
 
-const ProjectsCards = ({ children, img, alt, link }: ProjectProps) => {
+const ProjectsCards = ({
+  children,
+  img,
+  alt,
+  gitHubLink,
+  demoLink,
+}: ProjectProps) => {
   // Used to reference the parent element we want the drawer to appear from
   const ref = useRef<HTMLDivElement>(null);
-  // The container that holds the reference to the parent element.
   const [container, setContainer] = useState<HTMLElement | null>(null);
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const drawerRef = useRef<HTMLDivElement>(null);
 
-  // If ref is mounted and set to the parent element, the child element (drawer container) will set its reference to the parent element with the ref attribute.
+  // Set the container reference when parent ref mounts
   useEffect(() => {
     if (ref.current) {
       setContainer(ref.current);
     }
   }, []);
 
+  // Handle drawer toggle when the button is clicked
+  const handleDrawer = (e: SyntheticEvent) => {
+    setIsOpen(true);
+  };
+
+  // Function to handle clicks outside the drawer
+  const handleClickOutside = (e: MouseEvent) => {
+    if (drawerRef.current && !drawerRef.current.contains(e.target as Node)) {
+      setIsOpen(false); // Close the drawer if the click is outside
+    }
+  };
+
+  // Function to handles when the ESC key is clicked
+  const handleEscPressed = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      setIsOpen(false);
+    }
+  };
+
+  // Add event listener for clicks outside the drawer or ESC is pressed when it is open
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+      document.addEventListener("keydown", handleEscPressed);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscPressed);
+    }
+
+    // Cleanup the event listener when the component unmounts or drawer closes
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscPressed);
+    };
+  }, [isOpen]);
+
+  const title = (alt: string) => {
+    return alt.charAt(0).toUpperCase() + alt.slice(1);
+  };
+
   return (
     <div
       className="w-full rounded-md text-black relative overflow-hidden bg-custom-containerColor"
       ref={ref}
     >
-      <Drawer.Root container={container} direction="right">
-        <Drawer.Trigger className="absolute bottom-0 right-0 p-2 rounded-tl-sm text-custom-fontColor font-[700] bg-custom-containerColor outline-none">
-          <span className="m-2 p-2">View Details</span>
+      <Drawer.Root container={container} direction="right" handleOnly={true}>
+        <Drawer.Trigger
+          className="my-2 rounded-tl-sm text-custom-fontColor font-[700] bg-custom-containerColor outline-none cursor-pointer"
+          onClick={handleDrawer}
+        >
+          <span className="m-2 p-2">Learn More</span>
         </Drawer.Trigger>
 
         <Drawer.Portal>
-          <Drawer.Overlay className="" />
-          <Drawer.Content className="bg-red-500 absolute top-0 right-0 h-full w-[50%] rounded-l-[10px] flex flex-col">
-            {/* <Drawer.Handle className=""/> */}
+          <Drawer.Overlay />
+          <Drawer.Content
+            className="absolute z-10 top-0 right-0 h-full w-[50%] rounded-l-[10px] flex flex-col bg-blue-300/100"
+            ref={drawerRef}
+          >
             <div className="flex flex-col text-center m-3">
-              <Drawer.Title className="">Are you absolutely sure?</Drawer.Title>
-              <Drawer.Description>
-                This action cannot be undone.
+              <Drawer.Title className="text-left font-[700] text-[2em]">
+                {title(alt)}
+              </Drawer.Title>
+              <Drawer.Description className="flex text-left font-[500]" asChild>
+                {children.description}
               </Drawer.Description>
             </div>
 
-            <div>
-              <Button>Submit</Button>
-              <Drawer.Close asChild>
-                <Button variant="outline">Cancel</Button>
-              </Drawer.Close>
+            <div className="flex justify-center absolute bottom-0 m-auto w-full">
+              <div className="flex gap-2 mb-2">
+                <a
+                  className="cursor-pointer border rounded-sm p-1"
+                  href={gitHubLink}
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+                <a
+                  className="cursor-pointer border rounded-sm p-1"
+                  href={demoLink}
+                >
+                  Live Demo
+                </a>
+              </div>
             </div>
           </Drawer.Content>
         </Drawer.Portal>
       </Drawer.Root>
 
       {/* Inner container */}
-      <div className="flex flex-col m-2">
+      <div className="flex flex-col m-2 z-[-10]">
         {/* Image container */}
         <div className="w-full h-full rounded-tl-md rounded-tr-md">
-          <img src="/ProjectImgs/abode.png" alt={alt} className="rounded-md" />
+          <img
+            src={img}
+            alt={`${title(alt)} Project`}
+            className={`rounded-md ${isOpen ? "blur-[1px]" : ""}`}
+          />
         </div>
       </div>
     </div>
--- src/app/components/Projects/Projects.tsx
+++ src/app/components/Projects/Projects.tsx
@@ -1,12 +1,42 @@
+"use client";
 import React from "react";
-import ProjectsCards from "./Cards";
-import Stack from "./Stack";
-import { PageWrapper } from "../FramerMotion/PageWrapper";
-import { H1 } from "../TextComps";
+import ProjectsCards from "./ProjectCards";
+import { motion } from "framer-motion";
+import { variants } from "../FramerMotion/PageWrapper";
+
+const CurrentProjects: string[] = ["abode"];
 
 const Projects = () => {
   return (
-    <div
+    <motion.div variants={variants} className="w-full mb-20">
+      <div className="flex justify-between items-center gap-2">
+        <h1 className="text-[1.5em] font-[500] mb-2">Current Projects</h1>
+      </div>
+      <div className="flex flex-col items-center gap-10 w-full">
+        <ProjectsCards
+          alt="abode"
+          img="/ProjectImgs/abode.png"
+          gitHubLink="https://github.com/barreledout"
+        >
+          {{
+            description: (
+              <p>
+                A real estate website inspired by Zillow. Allows users to input
+                their property to receive a price evaluation on their home based
+                on comparable properties nearby.
+              </p>
+            ),
+          }}
+        </ProjectsCards>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Projects;
+
+{
+  /* <div
       className="flex flex-col justify-center text-center mt-[5rem] mb-[10rem]"
       id="projects"
     >
@@ -31,8 +61,5 @@ const Projects = () => {
           </ProjectsCards>
         </div>
       </PageWrapper>
-    </div>
-  );
-};
-
-export default Projects;
+    </div> */
+}
