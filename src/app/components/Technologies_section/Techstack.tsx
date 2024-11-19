c8bb32eec6b187cc3274fdcd562ec7006cc4d250
barreledout <rcalisaan7@gmail.com>
2024-11-19 14:51:38 -0800

    Made the view more button use the HoverMorebtn component.

--- src/app/components/Technologies_section/Techstack.tsx
+++ src/app/components/Technologies_section/Techstack.tsx
@@ -5,6 +5,7 @@ import Cards from "./TechCards";
 import { FaArrowRightLong } from "react-icons/fa6";
 import Modal from "../Modal";
 import H1 from "../H1";
+import HoverMoreBtn from "../HoverMoreBtn";
 
 const Techstack = () => {
   const CurrentStack: [string, string][] = [
@@ -26,33 +27,32 @@ const Techstack = () => {
       variants={variants}
       className="font-[400] text-custom-fontColor w-full"
     >
-      {/* possibly create a 'view more' to show more languages I know that popups a modal or new route? */}
-      <div className="flex justify-between items-center">
-        <H1 className="">Current Tech Stack</H1>
-        <Modal
-          _ModalTitle="Other Technologies"
-          _ModalTrigger={
-            <span className="flex items-center gap-2 font-[500]">
-              View More <FaArrowRightLong />
-            </span>
-          }
-        >
-          <div className="grid grid-cols-3 items-center gap-2 ">
-            {OtherStack.map(([lang, exp], index) => (
-              <div key={index}>
-                <Cards language={lang} exp={exp} className="rounded-sm" />
-              </div>
-            ))}
-          </div>
-        </Modal>
-      </div>
-
-      <div className="grid grid-cols-3 items-center gap-2">
-        {CurrentStack.map(([lang, exp], index) => (
-          <div key={index}>
-            <Cards language={lang} exp={exp} />
-          </div>
-        ))}
+      <div className="max-w-[650px]">
+        {/* possibly create a 'view more' to show more languages I know that popups a modal or new route? */}
+        <div className="flex justify-between items-center">
+          <H1 className="">Current Tech Stack</H1>
+          <Modal
+            _ModalTitle="Other Technologies"
+            _ModalTrigger={
+              <HoverMoreBtn title="View More"/>
+            }
+          >
+            <div className="grid grid-cols-3 items-center gap-2 ">
+              {OtherStack.map(([lang, exp], index) => (
+                <div key={index}>
+                  <Cards language={lang} exp={exp} className="rounded-sm" />
+                </div>
+              ))}
+            </div>
+          </Modal>
+        </div>
+        <div className="grid grid-cols-3 items-center gap-2">
+          {CurrentStack.map(([lang, exp], index) => (
+            <div key={index}>
+              <Cards language={lang} exp={exp} />
+            </div>
+          ))}
+        </div>
       </div>
     </motion.div>
   );
