164fdcd6cf2be538da51e09d36fab63dba4dd67e
barreledout <rcalisaan7@gmail.com>
2024-11-19 22:26:11 -0800

    Switched the icons to fit the theme and added a small animation when hovered and clicked.

--- src/app/components/Themebutton.tsx
+++ src/app/components/Themebutton.tsx
@@ -6,7 +6,6 @@ import useSound from "use-sound";
 
 import { Sun, Moon } from "lucide-react";
 
-
 const Themebutton = () => {
   const [mounted, setMounted] = useState(false);
   const { setTheme, resolvedTheme } = useTheme();
@@ -21,9 +20,9 @@ const Themebutton = () => {
     const theme = localStorage.getItem("theme");
 
     if (theme === "dark") {
-      return <Sun className="w-[40px] h-[50px] " />;
-    } else if (theme === "light" || "null") {
       return <Moon className="w-[40px] h-[40px]" />;
+    } else if (theme === "light" || "null") {
+      return <Sun className="w-[40px] h-[50px] " />;
     }
   }
 
@@ -45,16 +44,16 @@ const Themebutton = () => {
       className="flex justify-center items-center bg-transparent border-[none] w-[40px] h-[40px] cursor-pointer outline-none"
     >
       {resolvedTheme === "light" ? (
-        <Moon
-          className="text-fontColor [transition:color_0.2s_ease-in-out] hover:text-purple-500"
-          strokeWidth={"1.8px"}
-          size={"24px"}
-        />
-      ) : (
         <Sun
-          className="text-custom-fontColor [transition:color_0.2s_ease-in-out] pt-[2px] hover:text-yellow-500"
+          className="text-custom-fontColor [transition:all_0.2s_ease-in-out] pt-[2px] animate-themeIcon hover:text-yellow-500 hover:rotate-[20deg]"
           strokeWidth={"2px"}
           size={"25px"}
+        />
+      ) : (
+        <Moon
+          className="text-fontColor [transition:all_0.2s_ease-in-out] animate-themeIcon hover:text-purple-500 hover:rotate-[20deg]"
+          strokeWidth={"1.8px"}
+          size={"24px"}
         />
       )}
     </button>
